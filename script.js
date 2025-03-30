23cbbd0e6f7b344085d57d68ecbecf0d05b5ebb3
charan23-tech <mncharan1022@gmail.com>
2025-03-30 21:10:57 +0530

    changed the code

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-console.log("Website is working!");
+    document.addEventListener("DOMContentLoaded", function() {
+        document.querySelector("form").addEventListener("submit", function(event) {
+            event.preventDefault();
+            alert("Your message has been sent!");
+        });
+    });
